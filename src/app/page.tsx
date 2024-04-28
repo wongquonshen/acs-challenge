9d9b98765519f46a12a951bdde3c7137ac0d3a1e
Quon Shen <shaun.wong@involve.asia>
2024-04-28 18:02:33 +0800

    add question 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 'use client';
 
 import Header from "./_components/shared/header";
+import { twoSum } from "./question3";
 
 
 export default function Home() {
+
+  // log out question 3
+  // console.log('test 1: ', twoSum([2, 7, 11, 15], 9)); // Output: [1, 2]
+  // console.log('test 2: ', twoSum([4, 11, 17, 25], 21)); // Output: [1, 2]
+  // console.log('test 3: ', twoSum([0, 1, 2, 2, 3, 5], 4)); // Output: [2, 5]
+  // console.log('test 4: ', twoSum([-1, 0], -1)); // Output: [1, 2]
+  console.log('test 5: ', twoSum([2, 3, 4], 6)); // Output: [1, 3]
+
   return (
     <>
       <Header/>
---
+++ src/app/question3.ts
@@ -0,0 +1,23 @@
+export function twoSum(numbers: number[], target: number): number[] {
+    let left = 0;
+    let right = numbers.length - 1;
+    
+    while (left < right) {
+        // sum the first and last numbers in the array
+        let sum = numbers[left] + numbers[right];
+
+        // if the sum is same as target number provided
+        if (sum === target) {
+            // pass the array our left number and right number + 1
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            // if target more than the sum, wee increment the left
+            left++;
+        } else {
+            // otherwise, equal or less we reduce the right
+            right--;
+        }
+    }
+    
+    return []; // it shall not return here!
+}
