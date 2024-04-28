3cae10b5cca39ceb985fee134dd23402297d10f4
Quon Shen <shaun.wong@involve.asia>
2024-04-28 17:09:57 +0800

    add challenge 2 navbar

--- src/app/challenge-3/layout.tsx
+++ src/app/challenge-3/layout.tsx
@@ -1,14 +1,13 @@
-export default function Challenge1Layout({
+export default function Challenge2Layout({
     children,
   }: {
     children: React.ReactNode;
   }) {
     return (
       <>
-        <main className="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <main className="">
             {children}
         </main>
       </>
     );
 }
-  
